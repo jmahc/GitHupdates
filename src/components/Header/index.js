adc3faa10f5a07fa6815c2678bd4892dfc7d0029
Jordan McArdle <jordanmcardle@gmail.com>
2017-05-27 17:41:39 -0500

    Seperated out the components and scenes

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Icon from '../Icon'
+
+class Header extends Component {
+  render() {
+    return (
+      <div className='App-header'>
+        <Icon />
+        <h2>Welcome to React</h2>
+      </div>
+    )
+  }
+}
+
+export default Header
---
+++ src/components/Icon/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+
+class Icon extends Component {
+  render() {
+    return (
+      <img src={logo} className='App-logo' alt='logo' />
+    )
+  }
+}
+
+export default Icon
